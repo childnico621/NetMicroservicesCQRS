5600bf1ecbb6e7853f518787857cb785f79b3a15
Nicolas Agudelo Hincapie <nagudelo@paradigmasolutions.com>
2024-07-08 17:21:37 -0500

    configured rabbitMQ bus project

---
+++ TiendaServicios.RabbitMQ.Bus/BusRabbit/IBusRabbitEventBus.cs
@@ -0,0 +1,13 @@
+﻿using TiendaServicios.RabbitMQ.Bus.Commands;
+using TiendaServicios.RabbitMQ.Bus.Events;
+
+namespace TiendaServicios.RabbitMQ.Bus.BusRabbit
+{
+    internal interface IBusRabbitEventBus
+    {
+        Task Send<T>(T command)where T:Command;
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>(T @event) where T : Event where TH: IEventHandler<T>;
+    }
+}
