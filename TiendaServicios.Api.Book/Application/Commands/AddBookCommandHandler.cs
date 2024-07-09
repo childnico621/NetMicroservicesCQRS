6929512dd2ffbc448f3e9b668caec3b57c39c03b
Nicolas Agudelo Hincapie <nagudelo@paradigmasolutions.com>
2024-07-09 15:30:35 -0500

    rabbitMQ configured as class library and ready for use

--- TiendaServicios.Api.Book/Application/Commands/AddBookCommandHandler.cs
+++ TiendaServicios.Api.Book/Application/Commands/AddBookCommandHandler.cs
@@ -3,26 +3,36 @@ using TiendaServicios.Api.Book.Model;
 using TiendaServicios.Api.Book.Model.Request;
 using TiendaServicios.Api.Book.Model.Response;
 using TiendaServicios.Api.Book.Persistence;
+using TiendaServicios.RabbitMQ.Bus.BusRabbit;
+using TiendaServicios.RabbitMQ.Bus.QueueEvent;
 
 namespace TiendaServicios.Api.Book.Application.Commands
 {
     public class AddBookCommandHandler : IRequestHandler<AddBookRequestModel, AddBookResponseModel>
     {
         public readonly LibraryContext _context;
-
-        public AddBookCommandHandler(LibraryContext Context) => _context = Context;
+        private readonly IRabbitEventBus _eventBus;
+        public AddBookCommandHandler(LibraryContext Context, IRabbitEventBus eventBus)
+        {
+            _context = Context;
+            _eventBus = eventBus;
+        }
 
         public async Task<AddBookResponseModel> Handle(AddBookRequestModel request, CancellationToken cancellationToken)
         {
             var newBook = new BookMaterial
-            {                
-                Title= request.Title,
-                Published= request.Published,
+            {
+                Title = request.Title,
+                Published = request.Published,
                 AuthorId = request.AuthorId
             };
+            var dbBook = _context.Library.FirstOrDefault(x => x.Title == newBook.Title && x.AuthorId == newBook.AuthorId);
+            if (dbBook != null) return new AddBookResponseModel { BookId = dbBook.Id };
 
-            var result =  await _context.Library.AddAsync(newBook);
+            var result = await _context.Library.AddAsync(newBook);
             await _context.SaveChangesAsync();
+
+            _eventBus.Publish(new EmailQueueEvent("childnico621@gmail.com", request.Title, "this content is a example"));
 
             return new AddBookResponseModel
             {
